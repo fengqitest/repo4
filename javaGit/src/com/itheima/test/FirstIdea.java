604a38f73d213925752d95c9b3f1f48fa3632a00
time <test@time.com>
2019-07-20 12:38:08 +0800

    第一次添加

---
+++ javaGit/src/com/itheima/test/FirstIdea.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class FirstIdea {
+
+    public static void main(String[] args) {
+
+        System.out.println(" GIT IDEA  PROJECT");
+        System.out.println(" GIT IDEA  PROJECT");
+        System.out.println(" GIT IDEA  PROJECT");
+    }
+}
